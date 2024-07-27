6aaca67bd3aca9b65b241dbe6853e21ca293ff13
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-07-26 22:32:55 -0300

    Init config project

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,51 @@
-""" Modulo Principal """
+"""modulo principal do aplicativo
+
+Returns:
+    Função: Responsavel pela execução do aplicativo. Chama e estrutura todos
+    modulos.
+"""
 import os
 
 from flask import Flask
 
-def min_app():
-    """ Inicializa o flask
-    Retorna:
-        instância do flask
-    """
-    app = Flask(__name__)
-    return app
 
-def create_app():
-    """ Inicializa o app
-    Retorna:
-        Obj app
-    """
-    app = min_app()
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists, for bd work.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    # Import and run db.
+    from . import db
+    db.init_app(app)
+
+    # Import and run autentication app
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    # Import and run blog app, set index to '/' url.
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
